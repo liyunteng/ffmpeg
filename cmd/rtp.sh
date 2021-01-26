aba0f6fa3910f52b76f782a6167ba153f3e6f2ae
liyunteng <li_yunteng@163.com>
2021-01-26 20:44:49 +0800

    update rtp.sh

--- cmd/rtp.sh
+++ cmd/rtp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ffmpeg -re -stream_loop -1 -i abc.ts -vcodec copy -an -f rtp rtp://127.0.0.1:12345 > abc.sdp
-
 # PLAY
 # ffplay -protocol_whitelist="file,rtp,udp" abc.sdp
+
+
+# ffmpeg -re -stream_loop -1 -i abc.ts -vcodec copy -an -f rtp_mpegts rtp://127.0.0.1:12345
+# ffplay rtp://127.0.0.1:12345
