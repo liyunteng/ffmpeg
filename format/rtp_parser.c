4ff03c0cc6f98f42519caac15d7a2cc3b54533ed
liyunteng <li_yunteng@163.com>
2021-05-28 17:46:37 +0800

    update rtp_parser.c

--- format/rtp_parser.c
+++ format/rtp_parser.c
@@ -45,7 +45,106 @@ typedef struct {
     uint32_t ssrc;
 } rtp_fix_header;
 
-int
+    int
+rtp_parse_file(const char *filename)
+{
+    FILE *fp = NULL;
+    uint8_t buf[8*1024] = {0};
+    int nread, i;
+    rtp_fix_header *rtp = NULL;
+    FILE *ofp = NULL;
+    int cnt      = 0;
+
+
+    ofp = fopen("output.h264", "wb");
+    if (!ofp) {
+        printf("fopen output.h264 failed\n");
+        return -1;
+    }
+
+    fp = fopen(filename, "rb");
+    if (!fp) {
+        printf("fopen %s failed\n", filename);
+        return -1;
+    }
+
+    printf(" count | payload |  timestamp  | seqnum | pkt_size | ssrc\n");
+    while (!feof(fp)) {
+        memset(buf, 0, sizeof(buf));
+
+
+        nread = fread(buf, 1, sizeof(buf), fp);
+        if (nread != sizeof(buf)) {
+            printf("fread failed: %d\n", nread);
+            return -1;
+        }
+
+        for (i = 0; i < sizeof(buf) - 4; i++) {
+            if (buf[i] == 0x80 && buf[i+1] == 0x7f) {
+                rtp = (rtp_fix_header *)(buf+i);
+                char payload_str[20];
+                switch (rtp->payload) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                    case 10:
+                    case 11:
+                    case 12:
+                    case 13:
+                    case 14:
+                    case 15:
+                    case 16:
+                    case 17:
+                    case 18:
+                        sprintf(payload_str, "Audio");
+                        break;
+                    case 31:
+                        sprintf(payload_str, "H.261");
+                        break;
+                    case 32:
+                        sprintf(payload_str, "MPV");
+                        break;
+                    case 33:
+                        sprintf(payload_str, "MP2T");
+                        break;
+                    case 34:
+                        sprintf(payload_str, "H.263");
+                        break;
+                    case 96:
+                        sprintf(payload_str, "H.264");
+                        break;
+                    case 97:
+                        sprintf(payload_str, "AAC");
+                        break;
+                    default:
+                        sprintf(payload_str, "0x%x", rtp->payload);
+                        break;
+                }
+                uint32_t timestamp = ntohl(rtp->timestamp);
+                uint32_t seq       = ntohs(rtp->seq_no);
+
+                printf(" %5d | %7s | %11u | %6d | %8d | %10u\n", cnt, payload_str,
+                        timestamp, seq, 0, rtp->ssrc);
+                cnt++;
+            }
+
+        }
+
+
+    }
+
+
+    return 0;
+}
+
+    int
 rtp_parser(int port)
 {
     int fd;
@@ -77,59 +176,59 @@ rtp_parser(int port)
         pkt_size =
             recvfrom(fd, buf, 4096, 0, (struct sockaddr *)&remote, &remote_len);
         if (pkt_size > 0) {
-            char payload_str[10];
+            char payload_str[20];
             rtp_fix_header *rtp_header = NULL;
 
             rtp_header = (rtp_fix_header *)buf;
             switch (rtp_header->payload) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 17:
-            case 18:
-                sprintf(payload_str, "Audio");
-                break;
-            case 31:
-                sprintf(payload_str, "H.261");
-                break;
-            case 32:
-                sprintf(payload_str, "MPV");
-                break;
-            case 33:
-                sprintf(payload_str, "MP2T");
-                break;
-            case 34:
-                sprintf(payload_str, "H.263");
-                break;
-            case 96:
-                sprintf(payload_str, "H.264");
-                break;
-            case 97:
-                sprintf(payload_str, "AAC");
-                break;
-            default:
-                sprintf(payload_str, "other: %d", rtp_header->payload);
-                break;
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    sprintf(payload_str, "Audio");
+                    break;
+                case 31:
+                    sprintf(payload_str, "H.261");
+                    break;
+                case 32:
+                    sprintf(payload_str, "MPV");
+                    break;
+                case 33:
+                    sprintf(payload_str, "MP2T");
+                    break;
+                case 34:
+                    sprintf(payload_str, "H.263");
+                    break;
+                case 96:
+                    sprintf(payload_str, "H.264");
+                    break;
+                case 97:
+                    sprintf(payload_str, "AAC");
+                    break;
+                default:
+                    sprintf(payload_str, "other: %d", rtp_header->payload);
+                    break;
             }
             uint32_t timestamp = ntohl(rtp_header->timestamp);
             uint32_t seq       = ntohs(rtp_header->seq_no);
 
             printf(" %5d | %7s | %11u | %6d | %8d | %10u\n", cnt, payload_str,
-                   timestamp, seq, pkt_size, rtp_header->ssrc);
+                    timestamp, seq, pkt_size, rtp_header->ssrc);
             cnt++;
         }
     }
@@ -138,17 +237,19 @@ rtp_parser(int port)
     return 0;
 }
 
-int
+    int
 main(int argc, char *argv[])
 {
     if (argc < 1) {
         printf("Usage: %s <port>\n", argv[0]);
         return -1;
     }
-
+#if 0
     int i;
     for (i = 1; i < argc; i++) {
         rtp_parser(atoi(argv[1]));
     }
+#endif
+    rtp_parse_file(argv[1]);
     return 0;
 }
